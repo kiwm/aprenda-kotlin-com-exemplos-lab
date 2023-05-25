e3b9ea76db7482b747e0bb7e542c4d0e65a6ca92
Joao Pedro Carvalho Malta Pedrozo <45667697+kiwm@users.noreply.github.com>
2023-05-25 11:06:02 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,32 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val name: String, val matricula: String) {
+    
+    override fun toString(): String {
+        return name
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+    	usuario.forEach{inscritos.add(it)}
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val geometria = ConteudoEducacional("geometria", 60, Nivel.INTERMEDIARIO)
+    val geometria2 = ConteudoEducacional("geometria2", 60, Nivel.DIFICIL)
+    val formacao = Formacao("Mestre em geometria",listOf(geometria, geometria2))
+    val usuario1 = Usuario("name1", "matricula1")
+    val usuario2 = Usuario("name2", "matricula2")
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    println(formacao.inscritos.toString())
+    println(formacao.conteudos)
 }
